eba05afd8eedd2fe7ec8404d82e531df288afdeb
Vincent Esche <regexident@gmail.com>
2017-06-18 14:53:34 +0200

    Initial Commit

---
+++ Rainbows/Rainbows.h
@@ -0,0 +1,19 @@
+//
+//  Rainbows.h
+//  Rainbows
+//
+//  Created by Vincent Esche on 6/18/17.
+//  Copyright © 2017 Vincent Esche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rainbows.
+FOUNDATION_EXPORT double RainbowsVersionNumber;
+
+//! Project version string for Rainbows.
+FOUNDATION_EXPORT const unsigned char RainbowsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rainbows/PublicHeader.h>
+
+
